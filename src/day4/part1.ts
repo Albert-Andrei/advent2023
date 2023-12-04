ff1e93b6df2f9410d99b88c9bd18f3b425a047c6
Albert-Andrei <albert.m.wiredelta@gmail.com>
2023-12-04 09:19:31 +0100

    feat: day4 part1

---
+++ src/day4/part1.ts
@@ -0,0 +1,43 @@
+export function getResult(line: string) {
+  const matches: number[] = [];
+  const cardLine = line.split(':');
+
+  const cardId = cardLine[0].slice(5);
+  const cardValues = cardLine[1].split('|');
+
+  const winningArray = cardValues[0].split(' ').filter((c) => c !== '');
+  const otherNumbers = cardValues[1].split(' ').filter((c) => c !== '');
+
+  winningArray.map((numberStr) => {
+    if (otherNumbers.includes(numberStr)) {
+      matches.push(Number(numberStr));
+    }
+  });
+
+  let points = 0;
+
+  if (matches.length > 0) {
+    points = 1; // Start with 1 point for the first match
+    for (let i = 1; i < matches.length; i++) {
+      points *= 2; // Double points for each subsequent match
+    }
+  }
+
+  return points;
+}
+
+// @ts-ignore
+const fs = require('node:fs');
+const path = require('path').resolve(__dirname, './input.txt');
+
+fs.readFile(path, 'utf8', (err: unknown, data: string) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const result = data.split('\n').map(getResult);
+  const sum = result.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+  console.log(sum);
+});
